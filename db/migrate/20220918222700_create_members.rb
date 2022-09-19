58908727f4c1ab175a0d1aed3ce68ff7b2bb88bf
TrevorHatch <TrevorHatch@tamu.edu>
2022-09-19 23:40:57 +0000

    changed some attribute names

--- db/migrate/20220918222700_create_members.rb
+++ db/migrate/20220918222700_create_members.rb
@@ -1,7 +1,7 @@
 class CreateMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :members do |t|
-      t.string :user
+      t.string :username
       t.string :password
       t.string :f_name
       t.string :l_name
