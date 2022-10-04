698689d10b1933bca292c104c2b553518bea369e
root <TrevorHatch@tamu.edu>
2022-10-04 14:30:11 +0000

    updated migrations to reflect revised data design

--- db/migrate/20220918222409_create_attendances.rb
+++ db/migrate/20220918222409_create_attendances.rb
@@ -1,13 +1,9 @@
 class CreateAttendances < ActiveRecord::Migration[6.1]
   def change
     create_table :attendances do |t|
-      t.datetime :arrival_time
+      t.boolean :tardy
 
       t.timestamps
     end
-    #add_reference :attendances, :members, foreign_key: true
-    #add_reference  :members, :attendances, foreign_key: true
-    #add_reference :attendances, :events, foreign_key: true
-    #add_reference  :events, :attendances, foreign_key: true
     end
 end
