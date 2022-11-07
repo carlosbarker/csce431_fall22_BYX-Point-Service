00e51bba31e8fbdd9275f483bfe714781e1dfcad
TrevorHatch <108768741+TrevorHatch@users.noreply.github.com>
2022-11-06 19:49:12 -0600

    Update 20220918222700_create_members.rb

--- db/migrate/20220918222700_create_members.rb
+++ db/migrate/20220918222700_create_members.rb
@@ -2,7 +2,7 @@ class CreateMembers < ActiveRecord::Migration[6.1]
   def change
     create_table :members do |t|
       ## Personal info
-      t.string :email, null: false, default: ""
+      t.string :email
       t.string :full_name
       t.string :card_id
       t.string :phone
