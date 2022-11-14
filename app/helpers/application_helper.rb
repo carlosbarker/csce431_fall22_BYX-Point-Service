c30cff1405f3fdc3af912d16df7bb895ffd5a84e
carlosbarker <carlosbarker@outlook.com>
2022-11-14 11:47:25 -0600

    CSS files cleanup, is_admin() still pending

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   def is_admin(email)
+    #@member = Member.where(email: email)
+
+    #if (@member)
+    #  return true
+    #else
+    #  return false
+    #end
+
     return true
   end
 end
