2428752d68ee7dfd22387a6b3e50fc2dbcb5a653
trevorhatch <trevorhatch@tamu.edu>
2022-11-14 19:44:51 +0000

    working on card swipes upload

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,7 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @events_list = Event.order('datetime DESC')
+    @meetings_list = Event.order('datetime DESC')
   end
 end
